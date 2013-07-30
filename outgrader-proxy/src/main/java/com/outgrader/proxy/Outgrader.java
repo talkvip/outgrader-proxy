3cf1951819402e74d61105e0aeac90a7ffefafaf
Nikolay Lagutko <nikolay.lagutko@mail.com>
2013-07-30 19:45:11 +0300

    Main entry point

---
+++ outgrader-proxy/src/main/java/com/outgrader/proxy/Outgrader.java
@@ -0,0 +1,22 @@
+package com.outgrader.proxy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Main entry point to application
+ * 
+ * @author Nikolay Lagutko (nikolay.lagutko@mail.com)
+ * @since 0.1.0
+ */
+public class Outgrader {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(Outgrader.class);
+
+	public static void main(String[] args) {
+		LOGGER.info("Starting Ougrader-Runner application");
+		
+		LOGGER.info("Closing Outgrader-Runner application");
+	}
+
+}
