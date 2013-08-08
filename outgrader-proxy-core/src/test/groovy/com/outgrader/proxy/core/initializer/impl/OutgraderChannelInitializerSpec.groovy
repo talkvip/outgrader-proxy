635d74954e22319c6c0148e4e7f8eede57739918
Nikolay Lagutko <nikolay.lagutko@mail.com>
2013-08-08 15:58:49 +0300

    Tests on OutgraderChannelInitializer

---
+++ outgrader-proxy-core/src/test/groovy/com/outgrader/proxy/core/initializer/impl/OutgraderChannelInitializerSpec.groovy
@@ -0,0 +1,43 @@
+package com.outgrader.proxy.core.initializer.impl
+
+import io.netty.channel.ChannelPipeline
+import io.netty.channel.socket.SocketChannel
+import io.netty.handler.codec.http.HttpObjectAggregator
+import io.netty.handler.codec.http.HttpServerCodec
+import spock.lang.Specification
+
+import com.outgrader.proxy.core.handler.IOutgraderFrontendHandler
+
+/**
+ * @author Nikolay Lagutko (nikolay.lagutko@mail.com)
+ * @since 0.2.0-SNAPSHOT
+ *
+ */
+class OutgraderChannelInitializerSpec extends Specification {
+
+	OutgraderChannelInitializer initializer = new OutgraderChannelInitializer()
+
+	ChannelPipeline pipeline = Mock(ChannelPipeline)
+
+	SocketChannel channel = Mock(SocketChannel)
+
+	IOutgraderFrontendHandler handler = Mock(IOutgraderFrontendHandler)
+
+	def setup() {
+		initializer.frontendHandler = handler
+	}
+
+	def "check channel handlers"() {
+		when:
+		initializer.initChannel(channel)
+
+		then:
+		1 * pipeline.addLast(_ as String, _ as IOutgraderFrontendHandler)
+		1 * pipeline.addLast(_ as String, _ as HttpServerCodec)
+		1 * pipeline.addLast(_ as String, _ as HttpObjectAggregator)
+
+		1 * channel.pipeline() >> pipeline
+
+		0 * _._
+	}
+}
