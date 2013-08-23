20281532e2be3ae4a7740093bd298526c401e03d
Nikolay Lagutko <nikolay.lagutko@mail.com>
2013-08-23 13:50:19 +0300

    FilterUtils template

---
+++ outgrader-proxy-utils/src/main/java/com/ougrader/proxy/filter/FilterUtils.java
@@ -0,0 +1,14 @@
+package com.ougrader.proxy.filter;
+
+/**
+ * @author Nikolay Lagutko (nikolay.lagutko@mail.com)
+ * @since 0.4.0-SNAPSHOT
+ * 
+ */
+public final class FilterUtils {
+
+	private FilterUtils() {
+
+	}
+
+}
